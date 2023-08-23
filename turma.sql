8703c6a2e7d432379ed38b63aeb453f7eb078976
Adan Cardoso <125130778+adancard@users.noreply.github.com>
2023-08-23 11:18:49 -0300

    Add files via upload

---
+++ turma.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Aluno  (
+    Matricula INT PRIMARY KEY NOT NULL,
+    Nome VARCHAR(50) NOT NULL,
+    DataNascimento DATE NOT NULL,
+    Email VARCHAR(100) 
+ );
+ 
+ CREATE TABLE Matricula (
+    idMatricula INT PRIMARY KEY NOT NULL,
+    NomeAluno VARCHAR(50) NOT NULL,
+    SiglaCurso VARCHAR(10) NOT NULL,
+    DataInicio DATE NOT NULL,
+    FOREIGN KEY (idMatricula) REFERENCES Aluno(Matricula)
+ );
